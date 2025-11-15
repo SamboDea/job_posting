1daf0965bae7fa12462c3901f08cbb46c6eaaf48
Dea Sambo <sambodea54@gmail.com>
2025-11-15 23:35:10 +0700

    add

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -33,10 +33,10 @@ const Header = () => {
             className="collapse navbar-collapse justify-content-end gap-4"
             id="navbarNavAltMarkup"
           >
-            <Link className="login text-docoration-none" to="/features">
+            <Link className="login text-docoration-none" to="/signin">
               Sign In
             </Link>
-            <Link className="sign-up" to="/mobile-app">
+            <Link className="sign-up" to="/signup">
               Sign Up
             </Link>
             <ThemeToggle />
--- src/routes/WebsiteRoute.jsx
+++ src/routes/WebsiteRoute.jsx
@@ -1,19 +1,30 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import Footer from "../layouts/Footer";
 import Home from "../pages/website/Home";
 import Header from "../layouts/Header";
 import Theme from "../features/theme/components/Theme";
+import SignIn from "../features/auth/components/SignIn";
+import SignUp from "../features/auth/components/SignUp";
 
 const WebsiteRoutes = () => {
   return (
     <Theme>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Home />
+              <Footer />
+            </>
+          }
+        />
+
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
-      <Footer />
     </Theme>
   );
 };
